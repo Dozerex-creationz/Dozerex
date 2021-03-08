c425114a37a6734cb1bbec5b27e50b669d948183
Dhakshin Krishna <78342331+Dozerex-creationz@users.noreply.github.com>
2021-03-08 19:33:18 +0530

    Delete As8.java

--- As8.java
+++
@@ -1,9 +0,0 @@
-class As8
-{
- public static void main(String args[])
-	{	int x=0;
-		int a=Integer.parseInt(args[0]);
-		x=(a%10)+(a/1000);
-		System.out.println("the sum of the 1st and 4th digit is :"+x);
-	}
-}
